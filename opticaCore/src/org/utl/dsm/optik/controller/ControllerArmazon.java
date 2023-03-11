2cf69a91d767f5ab4b891af24275f9dba297ab33
Lalo L <thunderchihuahua2013@gmail.com>
2023-03-11 15:26:35 -0600

    ControllerArmazon comentado 1.0

--- opticaCore/src/org/utl/dsm/optik/controller/ControllerArmazon.java
+++ opticaCore/src/org/utl/dsm/optik/controller/ControllerArmazon.java
@@ -8,7 +8,6 @@ package org.utl.dsm.optik.controller;
  *
  * @author garni
  */
-
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -26,109 +25,108 @@ import org.utl.dsm.optik.model.Producto;
  * @author ferna13
  */
 public class ControllerArmazon {
+    // Método que inserta un objeto Armazon en la base de datos y devuelve su id
+
     public int insertar(Armazon armazon) throws Exception {
+// Paso 1: Definir la consulta a la base de datos
         String query = "call insertarArmazon(?,?,?,?,?,?,?,?,?,?,?,?,?)";
         int idProductoG = 0;
         int idArmazonG = 0;
         String codigoBarrasG = "";
         ConexionMySQL conexion = new ConexionMySQL();
         Connection conn = conexion.open();
-        
-        //Paso 3: Generar un objeto que permita preparar la llamada al procedure
+
+        // Paso 2: Preparar la llamada al procedure con los parámetros necesarios
         CallableStatement cstmt = conn.prepareCall(query);
         cstmt.setString(1, armazon.getProducto().getNombre());
         cstmt.setString(2, armazon.getProducto().getMarca());
         cstmt.setDouble(3, armazon.getProducto().getPrecioCompra());
         cstmt.setDouble(4, armazon.getProducto().getPrecioVenta());
         cstmt.setInt(5, armazon.getProducto().getExistencias());
-        
-        
+
         cstmt.setString(6, armazon.getModelo());
         cstmt.setString(7, armazon.getColor());
         cstmt.setString(8, armazon.getDimensiones());
         cstmt.setString(9, armazon.getDescripcion());
         cstmt.setString(10, armazon.getFotografia());
-        
+
+// Paso 3: Registrar los parámetros de salida del procedure
         cstmt.registerOutParameter(11, Types.INTEGER);
         cstmt.registerOutParameter(12, Types.INTEGER);
         cstmt.registerOutParameter(13, Types.VARCHAR);
-        
+
+// Paso 4: Ejecutar la consulta
         cstmt.executeUpdate();
-        
+
+// Paso 5: Obtener los valores de los parámetros de salida del procedure
         idProductoG = cstmt.getInt(11);
         idArmazonG = cstmt.getInt(12);
         codigoBarrasG = cstmt.getString(13);
-        
+
+// Paso 6: Actualizar el objeto Armazon con los valores obtenidos de la base de datos
         armazon.getProducto().setIdProducto(idProductoG);
         armazon.setIdArmazon(idArmazonG);
         armazon.getProducto().setCodigoBarras(codigoBarrasG);
         cstmt.close();
         conn.close();
         conexion.close();
-        
+
+// Paso 7: Devolver el id del objeto Armazon insertado
         return idArmazonG;
-        
+
     }
-    
-    
-    
-    
-    public void actualizar(Armazon armazon) throws Exception{
+
+    public void actualizar(Armazon armazon) throws Exception {
         //Preparar la sentencia sql
         String query = "call actualizarArmazon(?,?,?,?,?,?,?,?,?,?,?,?)";
-        
+
         //Paso 2: Conectar a la BD
         ConexionMySQL conexion = new ConexionMySQL();
         Connection conn = conexion.open();
-        
-         //Paso 3: Generar un objeto que permita preparar la llamada al procedure
+
+        //Paso 3: Generar un objeto que permita preparar la llamada al procedure
         CallableStatement cstmt = conn.prepareCall(query);
-        
+
         //cstmt.setString(1, armazon.getProducto().getCodigoBarras());
         cstmt.setString(1, armazon.getProducto().getNombre());
         cstmt.setString(2, armazon.getProducto().getMarca());
         cstmt.setDouble(3, armazon.getProducto().getPrecioCompra());
         cstmt.setDouble(4, armazon.getProducto().getPrecioVenta());
         cstmt.setInt(5, armazon.getProducto().getExistencias());
-        
+
         cstmt.setString(6, armazon.getModelo());
         cstmt.setString(7, armazon.getColor());
         cstmt.setString(8, armazon.getDimensiones());
         cstmt.setString(9, armazon.getDescripcion());
         cstmt.setString(10, armazon.getFotografia());
-       
-        cstmt.setInt(11 , armazon.getProducto().getIdProducto());
-        cstmt.setInt(12,armazon.getIdArmazon());
-        
-         //Paso 6: Ejecutar la llamada al procedure
+
+        cstmt.setInt(11, armazon.getProducto().getIdProducto());
+        cstmt.setInt(12, armazon.getIdArmazon());
+
+        //Paso 6: Ejecutar la llamada al procedure
         cstmt.executeUpdate();
-        
+
         // Paso 7 recuperar los parametros de salida 
-        
-        
         // Paso 8 insertar los valores en el objeto 
-        
-        
         // Paso 9 cerrar objetos de conexion 
         cstmt.close();
         conn.close();
         conexion.close();
-        
+
         // Paso 10 devolver o return el id generado
     }
-    
-    
-      public List<Armazon> getAll (String filtro) throws SQLException{
-        String query="SELECT * FROM vista_Armazon WHERE estatus="+filtro+";";
-        ConexionMySQL objConexion= new ConexionMySQL();
-        Connection conn=objConexion.open();
+
+    public List<Armazon> getAll(String filtro) throws SQLException {
+        String query = "SELECT * FROM vista_Armazon WHERE estatus=" + filtro + ";";
+        ConexionMySQL objConexion = new ConexionMySQL();
+        Connection conn = objConexion.open();
         PreparedStatement pstmt = conn.prepareStatement(query);
         List<Armazon> armazones = new ArrayList<>();
-        ResultSet rs= pstmt.executeQuery();
-        while(rs.next()){
-           
+        ResultSet rs = pstmt.executeQuery();
+        while (rs.next()) {
+
             Producto p = new Producto();
-            
+
             /*PRODUCTO*/
             p.setCodigoBarras(rs.getString("codigoBarras"));
             p.setNombre(rs.getString("nombre"));
@@ -137,8 +135,8 @@ public class ControllerArmazon {
             p.setPrecioVenta(rs.getDouble("precioVenta"));
             p.setExistencias(rs.getInt("existencias"));
             p.setIdProducto(rs.getInt("idProducto"));
-           
-             Armazon a = new Armazon();
+
+            Armazon a = new Armazon();
             /*ARMAZON*/
             a.setProducto(p);
             a.setIdArmazon(rs.getInt("idArmazon"));
@@ -148,89 +146,72 @@ public class ControllerArmazon {
             a.setDescripcion(rs.getString("descripcion"));
             a.setFotografia(rs.getString("fotografia"));
             a.setEstatus(rs.getInt("estatus"));
-            
+
             armazones.add(a);
-            
-            
-            
+
 //            a.setProducto(p);
 //           a.setIdArmazon(rs.getInt("idArmazon"));
 //            armazones.add(a);
-
         }
         rs.close();
         pstmt.close();
         conn.close();
         return armazones;
     }
-    
-    
-    public void estatusInactivo(Armazon armazon) throws Exception{
+
+    public void estatusInactivo(Armazon armazon) throws Exception {
         //Paso 1: Preparar la sentencia sql 
         String query = "call eliminarArmazon(?)";
-        
+
         //Paso 2: Conectar a la bd
         ConexionMySQL conexion = new ConexionMySQL();
         Connection conn = conexion.open();
-        
+
         //Paso 3 gENERAR UN OBJETO QUE PERMITA PREPARAR LA LLAMADA AL PROCEDURE 
         CallableStatement cstmt = conn.prepareCall(query);
-        
+
         //Paso 4 asignar los valores para cada parametro
-        cstmt.setInt(1 , armazon.getIdArmazon());
-        
+        cstmt.setInt(1, armazon.getIdArmazon());
+
         //Paso 6: Ejecutar la llamada al procedure
         cstmt.executeUpdate();
-        
+
         // Paso 7 recuperar los parametros de salida 
-        
-        
         // Paso 8 insertar los valores en el objeto 
-        
-        
         // Paso 9 cerrar objetos de conexion 
         cstmt.close();
         conn.close();
         conexion.close();
-        
+
         // Paso 10 devolver o return el id generado
-        
     }
-    
+
     //Cambiar estatus a activo 1
-     public void estatusActiv(Armazon armazon) throws Exception{
+    public void estatusActiv(Armazon armazon) throws Exception {
         //Paso 1: Preparar la sentencia sql 
         String query = "call activarArmazon(?)";
-        
+
         //Paso 2: Conectar a la bd
         ConexionMySQL conexion = new ConexionMySQL();
         Connection conn = conexion.open();
-        
+
         //Paso 3 gENERAR UN OBJETO QUE PERMITA PREPARAR LA LLAMADA AL PROCEDURE 
         CallableStatement cstmt = conn.prepareCall(query);
-        
+
         //Paso 4 asignar los valores para cada parametro
-        cstmt.setInt(1 , armazon.getIdArmazon());
-        
+        cstmt.setInt(1, armazon.getIdArmazon());
+
         //Paso 6: Ejecutar la llamada al procedure
         cstmt.executeUpdate();
-        
+
         // Paso 7 recuperar los parametros de salida 
-        
-        
         // Paso 8 insertar los valores en el objeto 
-        
-        
         // Paso 9 cerrar objetos de conexion 
         cstmt.close();
         conn.close();
         conexion.close();
-        
+
         // Paso 10 devolver o return el id generado
-        
     }
-    
-    
+
 }
-
-
